6460961556a5733197a1762ca9d29f0d47a09478
dianiputri03 <dianiputri930@gmail.com>
2024-09-03 17:01:35 +0700

    add recipe detail screen

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainScreen from './src/screens/MainScreen';
+import RecipeDetailScreen from './src/screens/RecipeDetailScreen';
 
 
 // function HomeScreen() {
@@ -45,6 +46,10 @@ function App() {
       <Stack.Navigator>
         <Stack.Screen name="MainScreen" component={MainScreen} options={{
           headerShown: false,
+        }}/>
+        <Stack.Screen name="RecipeDetailScreen" component={RecipeDetailScreen} options={{
+          title: 'Recipe Detail',
+          headerTransparent: true,
         }}/>
       </Stack.Navigator>
     </NavigationContainer>
